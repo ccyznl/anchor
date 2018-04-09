43b27d491b799a7487ee20c9a801fc4323000ecf
ccyznl <2845843140@qq.com>
2018-04-09 21:50:40 +0800

    添加一些基础类

---
+++ application/admin/lang/zh-cn/zh-cn.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adminstrator
+ * Date: 2018/4/9
+ * Time: 21:14
+ */
+return [
+    'User id'                                       => '会员ID',
+    'Username'                                      => '用户名',
+    'Nickname'                                      => '昵称',
+    'Password'                                      => '密码',
+    'Sign up'                                       => '注 册',
+    'Sign in'                                       => '登 录',
+    'Sign out'                                      => '注 销',
+    'Keep login'                                    => '保持会话',
+    'Guest'                                         => '游客',
+    'Welcome'                                       => '%s,你好',
+    'View'                                          => '查看',
+    'Add'                                           => '添加',
+    'Edit'                                          => '编辑',
+    'Del'                                           => '删除',
+    'Delete'                                        => '删除',
+    'Import'                                        => '导入',
+    'Detail'                                        => '详情',
+    'Multi'                                         => '批量更新',
+    'Setting'                                       => '设置',
+    'Move'                                          => '移动',
+    'Name'                                          => '名称',
+    'Status'                                        => '状态',
+];
+?>
